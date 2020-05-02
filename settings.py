ad2a633eb4bb0947de0bdea8abd80b87756a7f54
Joshua Andujar <andujar0218@gmail.com>
2020-05-02 05:05:08 -0400

    Initial commit

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings.
+        self.bg_color = (125, 125, 125)
+        self.caption = 'Alien Invasion'
+        self.screen_width = 1200
+        self.screen_height = 800
