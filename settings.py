9ec70f6251c86a58d6cde2b724e7f0a3bfd5edf0
Joshua Andujar <andujar0218@gmail.com>
2020-05-03 23:35:40 -0400

    Detecting alien-bullet collisions, alien-ship collisions, decrementing lives and resetting fleet on alien-ship collision.

--- settings.py
+++ settings.py
@@ -10,15 +10,18 @@ class Settings:
         self.screen_height = 800
 
         # Ship settings.
-        self.ship_speed = .3
+        self.ship_speed = 1.5
         self.ship_limit = 3
 
         # Bullet settings.
         self.bullet_speed = self.ship_speed * 1.5
-        self.bullet_width = 3
+        self.bullet_width = 300  # was 3
         self.bullet_height = 15
         self.bullet_color = (255, 0, 0)
         self.bullet_limit = 3
 
         # Alien settings.
         self.alien_speed = .5
+        self.fleet_drop_speed = 35
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
